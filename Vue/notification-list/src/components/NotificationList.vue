4735b0c23f54436bc979c30492ff8e6bf5a1141f
LaoLongBaBa <laolonggod@gmail.com>
2022-06-10 14:57:52 +0200

    work

---
+++ Vue/notification-list/src/components/NotificationList.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="hello">
+    <SingleNotification
+      v-for="(notification, index) in notifications"
+      v-bind:key="index"
+      :notifiDetails="notification"
+    />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import SingleNotification from "@/components/SingleNotifications.vue";
+
+export default {
+  name: "NotificationList",
+  components: {
+    SingleNotification,
+  },
+  data() {
+    return {
+      notifications: [],
+    };
+  },
+  beforeMount() {
+    axios.get("https://api.npoint.io/5526c5a502740142f4b2").then((response) => {
+      console.log(response.data.notifications);
+      this.notifications = response.data.notifications;
+    });
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ Vue/notification-list/src/components/SingleNotifications.vue
@@ -0,0 +1,55 @@
+<template>
+  <div
+    class="notification"
+    :class="{
+      info: notifiDetails.type == 'info',
+      warning: notifiDetails.type == 'warning',
+      text: notifiDetails.type == 'text',
+      readStatus: notifiDetails.read_status == true,
+    }"
+  >
+    <p>{{ TimeS(notifiDetails.timestamp) }}</p>
+    <p>{{ notifiDetails.description }}</p>
+  </div>
+</template>
+
+<style lang="scss">
+.notification {
+  margin: 0 auto;
+  padding: 5px;
+  box-sizing: border-box;
+  margin-bottom: 50px;
+  width: 700px;
+  background: white;
+}
+.info {
+  border: lightblue 5px solid;
+}
+.warning {
+  border: red 5px solid;
+}
+.text {
+  border: black 5px solid;
+}
+.readStatus {
+  opacity: 0.5;
+  color: rgba(0, 0, 0, 0.7);
+}
+</style>
+
+<script>
+import moment from "moment";
+
+export default {
+  name: "SingleNotification",
+  props: {
+    notifiDetails: Object,
+  },
+  methods: {
+    TimeS: function (value) {
+      return moment.unix(value).format("DD/MM - hh:mm");
+    },
+  },
+};
+</script>
+
